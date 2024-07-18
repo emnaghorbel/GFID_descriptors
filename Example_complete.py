a332923cda822422b990653b2e72ae29155439dd
Emna Ghorbel <85400167+emnaghorbel@users.noreply.github.com>
2024-07-18 21:07:25 +0100

    Update Example_complete.py

--- Example_complete.py
+++ Example_complete.py
@@ -5,7 +5,7 @@ from scipy.interpolate import CubicSpline
 from GFID_function import GFID_function
 from Reparametrage_euclidien2 import Reparametrage_euclidien2
 
-# Load and preprocess the image
+# Load and preprocess the image and extract the contour
 a = cv2.imread('pikachu.jpg')
 a_gray = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
 level = 0.1
@@ -18,7 +18,7 @@ for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
     plt.plot(contour[:, 0, 0], contour[:, 0, 1], 'r', linewidth=1)
 
-# Number of points in the shape
+# Number of points in the shape after resampling
 nbrp = 130
 
 # Resampling the contour
@@ -26,6 +26,7 @@ X1, Y1, L = Reparametrage_euclidien2(contour[:, 0, 0], contour[:, 0, 1], nbrp)
 
 # Convert to complex
 F = np.array([X1[i] + 1j * Y1[i] for i in range(nbrp)], dtype=np.complex128)
+#close the curve to obtain a contour (periodicity)
 F = np.append(F, X1[0] + 1j * Y1[0])
 
 # Parameters
@@ -38,19 +39,19 @@ q = 1
 npc = nbrp // 2
 A = np.zeros(nbrp, dtype=complex)
 
-# Compute A
+# Compute A (complex Discrete Fourier Transform)
 for k in range(nbrp):
     for n in range(nbrp):
         A[k] += F[n] * np.exp(-2j * np.pi * k * n / nbrp)
 
-# Centre de gravité
+# centroide
 GA = A[0] / nbrp
 
-# Invariants stables et complets
+# The calculation of the GFID 
 An = A[npc:]
 Ap = A[:npc]
 
-# Translation et tronquage
+# Translation et troncation
 Ac = np.zeros(nbrp, dtype=complex)
 Ac[:npc] = An
 Ac[npc:] = Ap
@@ -64,17 +65,20 @@ theta0 = np.angle(Anew[n0 + Nc])
 theta1 = np.angle(Anew[n1 + Nc])
 thetaN = np.angle(Anew)
 
-# Calcul des invariants
+# GFID Invariants
+#!st the power part
 Kk = np.zeros(2 * Nc + 1)
 for v in range(2 * Nc + 1):
     Kk[v] = v * (theta1 - theta0) + (n1 * theta0 - n0 * theta1) + (n0 - n1) * thetaN[v]
 
 E = np.exp(1j * Kk)
-
+#the final GFID descriptor
 IA = np.zeros(2 * Nc + 1, dtype=complex)
 for v in range(2 * Nc + 1):
     IA[v] = (abs(Anew[v])**(n0 - n1)) * (abs(Anew[n0 + Nc])**p) * (abs(Anew[n1 + Nc])**q) * E[v]
 
+
+#THE GFID INVERSION
 D = ((n0 - n1 + p) * (n0 - n1 + q)) - (p * q)
 alpha_0 = (n0 - n1 + q) / D
 beta_0 = -q / D
